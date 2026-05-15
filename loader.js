0d371903955ae396b83decb146a648c5620bbc2f
zikgrisakov <zikgrisakov125@gmail.com>
2026-05-15 17:52:37 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,8 +1,2 @@
-(function () {
-    alert('LOADER JS EXECUTED');
-
-    const box = document.createElement('div');
-    box.style = 'position:fixed;top:20px;left:20px;z-index:999999;background:red;color:white;padding:20px;font-size:20px;';
-    box.innerText = 'loader.js работает';
-    document.body.appendChild(box);
-})();
+alert('LOADER JS EXECUTED');
+console.log('LOADER JS EXECUTED');
