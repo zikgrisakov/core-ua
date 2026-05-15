461604d4fcc98b6a6923359a1de89e4aa476750c
zikgrisakov <zikgrisakov125@gmail.com>
2026-05-15 17:58:01 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,2 +1,13 @@
-alert('LOADER JS EXECUTED');
-console.log('LOADER JS EXECUTED');
+(function () {
+    'use strict';
+
+    const key = prompt('Введите ключ доступа');
+
+    if (key !== 'USER-111') {
+        alert('Неверный ключ');
+        return;
+    }
+
+    alert('Ключ принят, бот готов к запуску');
+    console.log('ACCESS GRANTED');
+})();
